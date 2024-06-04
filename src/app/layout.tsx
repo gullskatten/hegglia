7016b339c6c412f04bdc3aa2060c7acd02599be2
Espen G <espen_gud@msn.com>
2024-06-04 21:27:13 +0200

    Added anonymous analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Footer from './_components/footer';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -70,6 +71,7 @@ export default function RootLayout({
 
         <Footer />
         <SpeedInsights />
+        <Analytics />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={addOrganizationJsonLd()}
