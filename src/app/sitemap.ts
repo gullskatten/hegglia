c8c21286747a41f3cd2cef16a5bcd2056eb91e96
Espen G <espen_gud@msn.com>
2024-06-05 20:55:43 +0200

    sitemap is now generated from source

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://heggliaboligfelt.no',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://heggliaaboligfelt.no/boligfelt',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://heggliaboligfelt.no/galleri',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+}
